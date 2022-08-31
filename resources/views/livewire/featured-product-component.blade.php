3751595ec68881daffff563b8f6ce8de89ef8ac4
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-08-31 08:40:50 +0800

    Feature update gap

--- resources/views/livewire/featured-product-component.blade.php
+++ resources/views/livewire/featured-product-component.blade.php
@@ -1,46 +1,67 @@
 <div class="relative z-0 w-full pt-10 pb-10 text-center bg-gray-200">
 
     <div class="container mx-auto">
-        <h3 class="text-3xl font-semibold t-10">Popular Dishes</h3>
+        <h3 class="mt-10 text-3xl font-semibold">Popular Dishes</h3>
         <h1 class="text-5xl font-bold tracking-tighter text-orange-500">Our Delicious Food</h1>
-        <div class="grid grid-cols-2 gap-4 mt-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        <div class="grid grid-cols-2 gap-0 mt-10 mb-10 md:gap-2 lg:gap-3 md:grid-cols-3 lg:grid-cols-4">
 
 
-                <div class="p-5 pb-5 text-center bg-white rounded shadow-lg h-[300px] border">
+                <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
 
-                        <img src="{{ asset('assets/images/feature/food-img-1.png') }}" alt="" class="w-[150px] mx-auto">
+                        <img src="{{ asset('assets/images/feature/food-img-1.png') }}" alt="" class="w-full max-w-md mx-auto">
                         <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
                         <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
 
                 </div>
-                <div class="p-5 pb-5 text-center bg-white rounded shadow-lg h-[300px] border">
+                <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
 
-                        <img src="{{ asset('assets/images/feature/food-img-2.png') }}" alt="" class="w-[150px] mx-auto">
+                        <img src="{{ asset('assets/images/feature/food-img-2.png') }}" alt="" class="w-full max-w-md mx-auto">
                         <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
                         <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
 
                 </div>
-                <div class="p-5 pb-5 text-center bg-white rounded shadow-lg h-[300px] border">
+                <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
 
-                        <img src="{{ asset('assets/images/feature/food-img-3.png') }}" alt="" class="w-[150px] mx-auto">
+                        <img src="{{ asset('assets/images/feature/food-img-3.png') }}" alt="" class="w-full max-w-md mx-auto">
                         <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
                         <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
 
                 </div>
-                <div class="p-5 pb-5 text-center bg-white rounded shadow-lg h-[300px] border">
+                <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
 
-                        <img src="{{ asset('assets/images/feature/food-img-4.png') }}" alt="" class="w-[150px] mx-auto">
+                        <img src="{{ asset('assets/images/feature/food-img-4.png') }}" alt="" class="w-full max-w-md mx-auto">
                         <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
                         <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
 
                 </div>
-                <div class="p-5 pb-5 text-center bg-white rounded shadow-lg h-[300px] border">
+                <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
 
-                        <img src="{{ asset('assets/images/feature/food-img-5.png') }}" alt="" class="w-[150px] mx-auto">
+                        <img src="{{ asset('assets/images/feature/food-img-5.png') }}" alt="" class="w-full max-w-md mx-auto">
                         <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
                         <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
 
                 </div>
+                <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
+
+                    <img src="{{ asset('assets/images/feature/food-img-1.png') }}" alt="" class="w-full max-w-md mx-auto">
+                    <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
+                    <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
+
+            </div>
+            <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
+
+                    <img src="{{ asset('assets/images/feature/food-img-2.png') }}" alt="" class="w-full max-w-md mx-auto">
+                    <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
+                    <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
+
+            </div>
+            <div class="p-5 pb-5 text-center bg-white border rounded shadow-lg">
+
+                    <img src="{{ asset('assets/images/feature/food-img-3.png') }}" alt="" class="w-full max-w-md mx-auto">
+                    <h1 class="mt-5 text-2xl font-bold tracking-tighter capitalize">Delicious Food</h1>
+                    <p class="text-2xl text-orange-500 font-semitbold">$42.88</p>
+
+            </div>
 
         </div>
     </div>
