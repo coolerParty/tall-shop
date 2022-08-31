6035c486fa540438ecd5a71ab49c54c52e96d732
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-08-31 11:32:12 +0800

    review added

---
+++ app/Http/Livewire/HomeSliderComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HomeSliderComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.home-slider-component');
+    }
+}
--- resources/views/livewire/featured-product-component.blade.php
+++ resources/views/livewire/featured-product-component.blade.php
@@ -1,8 +1,8 @@
 <div class="relative z-0 w-full pt-10 pb-10 text-center bg-gray-200">
 
     <div class="container mx-auto">
-        <h3 class="mt-10 text-3xl font-semibold">Popular Dishes</h3>
-        <h1 class="text-5xl font-bold tracking-tighter text-orange-500">Our Delicious Food</h1>
+        <h3 class="mt-10 text-3xl font-semibold capitalize">Popular Dishes</h3>
+        <h1 class="text-5xl font-bold tracking-tighter text-orange-500 capitalize">Our Delicious Food</h1>
         <div class="grid grid-cols-2 gap-0 mt-10 mb-10 md:gap-2 lg:gap-3 md:grid-cols-3 lg:grid-cols-4">
 
 
