2e41c8a0e44668fe6936eecf12932e7c919112ff
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-09-02 10:04:48 +0800

    Rename Website

--- resources/views/components/nav-menu-component.blade.php
+++ resources/views/components/nav-menu-component.blade.php
@@ -4,8 +4,9 @@
         class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
         <div class="flex flex-row items-center justify-between p-4">
             <a href="#"
-                class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">Flowtrail
-                UI</a>
+                class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">
+                Fruidy
+            </a>
 
             <div class="flex">
                 <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
@@ -46,20 +47,20 @@
         <nav :class="{'flex': open, 'hidden': !open}"
             class="z-50 flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
 
-            <a class="z-50 px-4 py-2 mt-2 text-sm font-semibold  
-            dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 
-            dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 
+            <a class="z-50 px-4 py-2 mt-2 text-sm font-semibold
+            dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600
+            dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0
         hover:bg-orange-500 hover:text-white focus:bg-orange-600 focus:outline-none focus:shadow-outline
             @if(route('home') == url()->current())
-            focus:text-gray-100  focus:bg-orange-500 text-white bg-orange-500 
+            focus:text-gray-100  focus:bg-orange-500 text-white bg-orange-500
             @else hover:bg-gray-200 @endif"
                 href="{{ route('home') }}">Home</a>
-            <a class="z-50 px-4 py-2 mt-2 text-sm font-semibold  
-                        dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 
-                        dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 
+            <a class="z-50 px-4 py-2 mt-2 text-sm font-semibold
+                        dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600
+                        dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0
                     hover:bg-orange-500 hover:text-white focus:bg-orange-600 focus:outline-none focus:shadow-outline
                         @if(route('menu') == url()->current())
-                        focus:text-gray-100  focus:bg-orange-500 text-white bg-orange-500 
+                        focus:text-gray-100  focus:bg-orange-500 text-white bg-orange-500
                         @else hover:bg-gray-200 @endif"
                 href="{{ route('menu') }}">Menu</a>
             <a class="z-50 px-4 py-2 mt-2 text-sm font-semibold bg-transparent dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
