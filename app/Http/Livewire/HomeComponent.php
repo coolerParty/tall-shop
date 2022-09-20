1a47c07ab68cfc2bc1f09bee20e06843fef001a8
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-09-20 21:54:54 +0800

    Cart save to Database.

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Cart;
 
 class HomeComponent extends Component
 {
     public function render()
     {
+        if (Auth::check()) {
+            Cart::instance('cart')->restore(Auth::user()->email); // save cart to database using user email;
+            Cart::instance('wishlist')->restore(Auth::user()->email); // save wishlist to database using user email;
+        }
+
         return view('livewire.home-component')->layout('layouts.front');
     }
 }
