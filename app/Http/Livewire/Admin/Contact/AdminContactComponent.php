b3ac6775f59b4d9c087267b4940a9efe8bacb66b
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-09-20 19:09:29 +0800

    Admin Contact List Added

--- app/Http/Livewire/Admin/Contact/AdminContactComponent.php
+++ app/Http/Livewire/Admin/Contact/AdminContactComponent.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire\Admin\Contact;
 
+use App\Models\Contact;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class AdminContactComponent extends Component
 {
+    use AuthorizesRequests;
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.admin.contact.admin-contact-component')->layout('layouts.base');
+        $this->authorize('contact-show');
+
+        $contacts = Contact::orderBy('created_at', 'DESC')->paginate(10);
+        return view('livewire.admin.contact.admin-contact-component', ['contacts' => $contacts])->layout('layouts.base');
     }
 }
--- resources/views/livewire/contact-component.blade.php
+++ resources/views/livewire/contact-component.blade.php
@@ -9,32 +9,33 @@
             </div>
             <form class="flex flex-col justify-center m-2 md:m-0" wire:submit.prevent="sendMessage">
                 @if (Session::has('message'))
-        <div x-data="{ msg:'true'}">
-            <template x-if="msg">
-                <div class="w-full text-white bg-indigo-500">
-                    <div class="container flex items-center justify-between px-6 py-4 mx-auto">
-                        <div class="flex">
-                            <svg viewBox="0 0 40 40" class="w-6 h-6 fill-current">
-                                <path
-                                    d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z">
-                                </path>
-                            </svg>
+                <div x-data="{ msg:'true'}">
+                    <template x-if="msg">
+                        <div class="w-full text-white bg-indigo-500">
+                            <div class="container flex items-center justify-between px-6 py-4 mx-auto">
+                                <div class="flex">
+                                    <svg viewBox="0 0 40 40" class="w-6 h-6 fill-current">
+                                        <path
+                                            d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z">
+                                        </path>
+                                    </svg>
 
-                            <p class="mx-3">{{ Session::get('message') }}</p>
+                                    <p class="mx-3">{{ Session::get('message') }}</p>
+                                </div>
+
+                                <button @click="msg = '' "
+                                    class="p-1 transition-colors duration-200 transform rounded-md hover:bg-opacity-25 hover:bg-gray-600 focus:outline-none">
+                                    <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6 18L18 6M6 6L18 18" stroke="currentColor" stroke-width="2"
+                                            stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </button>
+                            </div>
                         </div>
-
-                        <button @click="msg = '' "
-                            class="p-1 transition-colors duration-200 transform rounded-md hover:bg-opacity-25 hover:bg-gray-600 focus:outline-none">
-                            <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M6 18L18 6M6 6L18 18" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </button>
-                    </div>
+                    </template>
                 </div>
-            </template>
-        </div>
-        @endif
+                @endif
                 <div class="flex flex-col">
                     <label for="name" class="hidden">Full Name</label>
                     @error('name')<div class="text-xs italic text-red-500">{{ $message }}</div>@enderror
@@ -47,7 +48,7 @@
                     <label for="email" class="hidden">Email</label>
                     @error('email')<div class="text-xs italic text-red-500">{{ $message }}</div>@enderror
                     <input type="email" name="email" id="email" placeholder="Email"
-                        class="px-3 py-3 mt-1 font-semibold text-gray-800 bg-white border border-gray-400 w-100 dark:bg-gray-800 dark:border-gray-700 focus:border-indigo-500 focus:outline-none"
+                        class="px-3 py-3 mt-1 font-semibold text-gray-800 bg-white border border-gray-400 w-100 dark:bg-gray-800 dark:border-gray-700 focus:border-indigo-500 focus:outline-none @error('email') border-red-400 focus:border-red-500 text-red-800 @enderror"
                         wire:model="email">
                 </div>
 
@@ -55,7 +56,7 @@
                     <label for="tel" class="hidden">Number</label>
                     @error('phone')<div class="text-xs italic text-red-500">{{ $message }}</div>@enderror
                     <input type="tel" name="tel" id="tel" placeholder="Telephone Number"
-                        class="px-3 py-3 mt-1 font-semibold text-gray-800 bg-white border border-gray-400 w-100 dark:bg-gray-800 dark:border-gray-700 focus:border-indigo-500 focus:outline-none"
+                        class="px-3 py-3 mt-1 font-semibold text-gray-800 bg-white border border-gray-400 w-100 dark:bg-gray-800 dark:border-gray-700 focus:border-indigo-500 focus:outline-none @error('phone') border-red-400 focus:border-red-500 text-red-800 @enderror"
                         wire:model="phone">
                 </div>
 
@@ -63,7 +64,7 @@
                     <label for="msg" class="hidden">Message</label>
                     @error('comment')<div class="text-xs italic text-red-500">{{ $message }}</div>@enderror
                     <textarea name="msg" id="msg" placeholder="Message" cols="30" rows="5"
-                        class="px-3 py-3 mt-1 font-semibold text-gray-800 bg-white border border-gray-400 w-100 dark:bg-gray-800 dark:border-gray-700 focus:border-indigo-500 focus:outline-none"
+                        class="px-3 py-3 mt-1 font-semibold text-gray-800 bg-white border border-gray-400 w-100 dark:bg-gray-800 dark:border-gray-700 focus:border-indigo-500 focus:outline-none @error('comment') border-red-400 focus:border-red-500 text-red-800 @enderror"
                         wire:model="comment"></textarea>
                 </div>
 
