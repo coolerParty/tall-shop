fce1e222ad40c6b25790814212ac9e0e2372e59d
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-10-09 21:39:50 +0800

    Minor updates

--- app/Http/Livewire/GalleryComponent.php
+++ app/Http/Livewire/GalleryComponent.php
@@ -8,44 +8,57 @@ use Livewire\Component;
 
 class GalleryComponent extends Component
 {
-    public $image_max_number = 12;
-    public $category_selected_id = 0;  // 0 means view all category
-    public $galleries_total = 0;
+    public $imageMaxNumber     = 12;
+    public $categorySelectedId = 0;   // 0 means view all category
+    public $galleriesTotal     = 0;
 
     public function viewMoreImage()
     {
-        $this->image_max_number = $this->image_max_number + 6;
+        $this->imageMaxNumber = $this->imageMaxNumber + 6;
     }
 
     public function selectCategory($cat_id)
     {
-        $this->category_selected_id = $cat_id;
+        $this->categorySelectedId = $cat_id;
+    }
+
+    public function getTotalCount()
+    {
+        return Gallery::where('active', true)
+            ->when($this->categorySelectedId <> 0, function ($q) {
+                return $q->where('category_id', $this->categorySelectedId);
+            })
+            ->count();
+    }
+
+    public function getGalleries()
+    {
+        return Gallery::with('category')
+            ->select('name', 'image', 'category_id')
+            ->where('active', true)
+            ->when($this->categorySelectedId <> 0, function ($q) {
+                return $q->where('category_id', $this->categorySelectedId);
+            })
+            ->take($this->imageMaxNumber)
+            ->orderBy('created_at', 'ASC')
+            ->get();
+    }
+
+    public function getCategories()
+    {
+        return Category::select('id', 'name')
+            ->orderBy('name', 'ASC')
+            ->get();
     }
 
     public function render()
     {
-        $this->galleries_total = Gallery::
-        where('active', true)
-        ->when($this->category_selected_id <> 0, function ($q) {
-            return $q->where('category_id', $this->category_selected_id);
-        })
-        ->count();
+        $this->galleriesTotal = $this->getTotalCount();
+        $galleries = $this->getGalleries();
+        $categories = $this->getCategories();
 
-        $galleries = Gallery::with('category')
-            ->select('name', 'image', 'category_id')
-            ->where('active', true)
-            ->when($this->category_selected_id <> 0, function ($q) {
-                return $q->where('category_id', $this->category_selected_id);
-            })
-            ->take($this->image_max_number)
-            ->orderBy('created_at', 'ASC')
-            ->get();
-
-        $categories = Category::select('id', 'name')
-            ->orderBy('name', 'ASC')
-            ->get();
         return view('livewire.gallery-component', [
-            'galleries' => $galleries, 'categories' => $categories
-        ])->layout('layouts.front');
+                        'galleries' => $galleries, 'categories' => $categories
+                    ])->layout('layouts.front');
     }
 }
