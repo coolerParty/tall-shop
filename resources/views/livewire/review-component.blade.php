578f61c6b1ccc0afc0977bee4494833b5ecc180f
Alex Meier <92195211+coolerParty@users.noreply.github.com>
2022-10-16 12:14:49 +0800

    Home Review and Featured Product optimized.

--- resources/views/livewire/review-component.blade.php
+++ resources/views/livewire/review-component.blade.php
@@ -8,40 +8,40 @@
                 class="p-3 transition duration-150 ease-in-out transform bg-white hover:shadow-lg hover:rounded hover:scale-102">
                 <div class="m-5">
                     <div class="flex space-x-0.5">
-                        <svg class="w-5 h-5 {{ ($review->count() != 0 && 1 <= ($review->sum('rating') / $review->count())) ? 'text-yellow-300' : 'text-gray-300' }}"
-                            fill="{{ ($review->count() != 0 && 1 <= ($review->sum('rating') / $review->count())) ? 'currentColor' : 'none' }}"
+                        <svg class="w-5 h-5 {{ ($review->rating != 0 && 1 <= $review->rating) ? 'text-yellow-300' : 'text-gray-300' }}"
+                            fill="{{ ($review->rating != 0 && 1 <= $review->rating) ? 'currentColor' : 'none' }}"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path
                                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
                             </path>
                         </svg>
-                        <svg class="w-5 h-5 {{ ($review->count() != 0 && 2 <= ($review->sum('rating') / $review->count())) ? 'text-yellow-300' : 'text-gray-300' }}"
-                            fill="{{ ($review->count() != 0 && 2 <= ($review->sum('rating') / $review->count())) ? 'currentColor' : 'none' }}"
+                        <svg class="w-5 h-5 {{ ($review->rating != 0 && 2 <= $review->rating) ? 'text-yellow-300' : 'text-gray-300' }}"
+                            fill="{{ ($review->rating != 0 && 2 <= $review->rating) ? 'currentColor' : 'none' }}"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path
                                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
                             </path>
                         </svg>
-                        <svg class="w-5 h-5 {{ ($review->count() != 0 && 3 <= ($review->sum('rating') / $review->count())) ? 'text-yellow-300' : 'text-gray-300' }}"
-                            fill="{{ ($review->count() != 0 && 3 <= ($review->sum('rating') / $review->count())) ? 'currentColor' : 'none' }}"
+                        <svg class="w-5 h-5 {{ ($review->rating != 0 && 3 <= $review->rating) ? 'text-yellow-300' : 'text-gray-300' }}"
+                            fill="{{ ($review->rating != 0 && 3 <= $review->rating) ? 'currentColor' : 'none' }}"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path
                                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
                             </path>
                         </svg>
-                        <svg class="w-5 h-5 {{ ($review->count() != 0 && 4 <= ($review->sum('rating') / $review->count())) ? 'text-yellow-300' : 'text-gray-300' }}"
-                            fill="{{ ($review->count() != 0 && 4 <= ($review->sum('rating') / $review->count())) ? 'currentColor' : 'none' }}"
+                        <svg class="w-5 h-5 {{ ($review->rating != 0 && 4 <= $review->rating) ? 'text-yellow-300' : 'text-gray-300' }}"
+                            fill="{{ ($review->rating != 0 && 4 <= $review->rating) ? 'currentColor' : 'none' }}"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path
                                 d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z">
                             </path>
                         </svg>
-                        <svg class="w-5 h-5 {{ ($review->count() != 0 && 5 <= ($review->sum('rating') / $review->count())) ? 'text-yellow-300' : 'text-gray-300' }}"
-                            fill="{{ ($review->count() != 0 && 5 <= ($review->sum('rating') / $review->count())) ? 'currentColor' : 'none' }}"
+                        <svg class="w-5 h-5 {{ ($review->rating != 0 && 5 <= $review->rating) ? 'text-yellow-300' : 'text-gray-300' }}"
+                            fill="{{ ($review->rating != 0 && 5 <= $review->rating) ? 'currentColor' : 'none' }}"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="1" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path
